8c88c1dc66e32bbd5c750add1471dc6ed55dbb95
Quinten Van Bever <quinten.vb.96@gmail.com>
2020-04-25 21:15:31 +0200

    basic styling + header; made flexgrid component

--- playground-ng/src/app/app-routing.module.ts
+++ playground-ng/src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FlexgridComponent } from './flexgrid/flexgrid.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [{ path: 'flexgrid', component: FlexgridComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- playground-ng/src/app/app.module.ts
+++ playground-ng/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FlexgridComponent } from './flexgrid/flexgrid.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, FlexgridComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
