16e6772eb3c4578db32d3b04e14a6d95b7f60ee6
Quinten Van Bever <quinten.vb.96@gmail.com>
2020-10-07 21:54:56 +0200

    Basic Rest API with MongoDB + Express

---
+++ express-mongodb/routes/subscribers.js
@@ -0,0 +1,81 @@
+const e = require("express");
+const express = require("express");
+const { update } = require("../models/subscriber");
+const subscriber = require("../models/subscriber");
+const router = express.Router();
+const Subscriber = require("../models/subscriber");
+
+// GET subscribers
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// GET 1 subscriber
+router.get("/:id", getSubscriber, (req, res) => {
+  res.send(res.subscriber);
+});
+
+// POST 1 subscriber
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// PUT 1 subscriber
+// * Only update what gets passed
+router.patch("/:id", getSubscriber, async (req, res) => {
+  if (req.body.name != null) {
+    res.subscriber.name = req.body.name;
+  }
+  if (req.body.subscribedToChannel != null) {
+    res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+  }
+
+  
+  try {
+    const updatedSubscriber = await res.subscriber.save();
+    res.json(updatedSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
+// DELETE 1 subscriber
+router.delete("/:id", getSubscriber, async (req, res) => {
+  try {
+    await res.subscriber.remove();
+    res.json("Deleted subscriber");
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+module.exports = router;
+
+// Middleware
+async function getSubscriber(req, res, next) {
+  let subscriber;
+  try {
+    subscriber = await Subscriber.findById(req.params.id);
+    if (subscriber == null) {
+      return res.status(404).json("Subscriber not found!");
+    }
+  } catch (err) {
+    return res.status(500).json({ message: err.message });
+  }
+  res.subscriber = subscriber;
+  next();
+}
